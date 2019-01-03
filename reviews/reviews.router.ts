0cb5e7651396dc7307b90405a07ea0f6d440b5e0
Miguel Nedel <miguel.nedel@mohva.com.br>
2019-01-03 16:59:52 -0200

    first commit

---
+++ reviews/reviews.router.ts
@@ -0,0 +1,32 @@
+import * as mongoose from 'mongoose'
+import { ModelRouter } from '../common/model-router'
+import * as restify from 'restify'
+import { NotFoundError } from 'restify-errors'
+import { Review } from './reviews.model'
+
+class ReviewsRouter extends ModelRouter<Review>{
+    constructor(){
+        super(Review)
+    }
+
+    envelope(document){
+        let resource = super.envelope(document)
+        const restId = document.restaurant._id ? document.restaurant._id : document.restaurant
+        resource._links.restaurant = `/restaurants/${restId}`
+        return resource
+    }
+
+    prepareOne(query: mongoose.DocumentQuery<Review, Review>): mongoose.DocumentQuery<Review, Review> {
+        return query.populate('user', 'name')
+                    .populate('restaurant', 'name')
+    }
+
+    applyRoutes(application: restify.Server) {
+        application.get(`${this.basePath}`, this.findAll)
+        application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
+        application.post(`${this.basePath}`, this.save)
+    }
+
+}
+
+export const reviewsRouter = new ReviewsRouter()
